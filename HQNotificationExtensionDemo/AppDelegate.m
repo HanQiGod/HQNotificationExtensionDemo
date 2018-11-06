a439a503cb0dfbe038ec86cf131f0a5790f54229
HanQiGod <1539841933@qq.com>
2018-11-06 10:47:53 +0800

    优化

--- HQNotificationExtensionDemo/AppDelegate.m
+++ HQNotificationExtensionDemo/AppDelegate.m
@@ -4,6 +4,8 @@
 //
 //  Created by Mr_Han on 2018/11/3.
 //  Copyright © 2018 Mr_Han. All rights reserved.
+//  CSDN <https://blog.csdn.net/u010960265>
+//  GitHub <https://github.com/HanQiGod>
 //
 
 #import "AppDelegate.h"
